d60afe423e12389f4c339fb99d17d11062f1ed4f
Ryan Luong <lumintriet@gmail.com>
2025-08-03 00:26:37 +0700

    Setup shadcn/ui.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col gap-4 justify-center items-center h-screen">
       <h1 className="text-4xl font-bold">Sparkle</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
