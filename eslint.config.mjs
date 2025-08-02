c8b696445254b9fa4970972cc62fa556b6778b54
Ryan Luong <lumintriet@gmail.com>
2025-08-03 00:19:11 +0700

    Add ESLint.

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import js from "@eslint/js";
+import eslintPluginNext from "@next/eslint-plugin-next";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import eslintPluginReactRefresh from "eslint-plugin-react-refresh";
+import { globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  globalIgnores(["**/*.d.ts", "**/dist/"]),
+  js.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      },
+      globals: { ...globals.browser, ...globals.serviceworker }
+    }
+  },
+  eslintPluginReactHooks.configs["recommended-latest"],
+  {
+    plugins: {
+      "react-refresh": eslintPluginReactRefresh
+    },
+    rules: {
+      "react-refresh/only-export-components": [
+        "error",
+        { allowExportNames: ["metadata"] }
+      ]
+    }
+  },
+  {
+    plugins: {
+      "@next/next": eslintPluginNext
+    },
+    /** @type {any} */
+    rules: {
+      ...eslintPluginNext.configs.recommended.rules,
+      ...eslintPluginNext.configs["core-web-vitals"].rules
+    }
+    // Used type assertion because the Severity level specified in the plugin for nextjs
+    // is not compatible with the one expected by typescript-eslint
+    // typescript-eslint expects literal "error" | "warn" | "off"
+    // @next/eslint-plugin-next uses the strings "error", "warn"
+  },
+  eslintConfigPrettier
+);
