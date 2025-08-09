5bbce925d95b7f3b996ba0407ef1e8b93ca26276
Ryan Luong <lumintriet@gmail.com>
2025-08-09 16:07:56 +0700

    Add auth schema for use with Better Auth library.

--- database/helpers/timestamps.ts
+++ database/helpers/timestamps.ts
@@ -1,12 +1,15 @@
+import { sql } from "drizzle-orm";
 import { integer } from "drizzle-orm/sqlite-core";
 
 const timestamps = {
   createdAt: integer("created_at", { mode: "timestamp" })
-    .$defaultFn(() => /* @__PURE__ */ new Date())
-    .notNull(),
-  updatedAt: integer("updated_at", { mode: "timestamp" })
-    .$defaultFn(() => /* @__PURE__ */ new Date())
     .notNull()
+    .default(sql`(strftime('%s', 'now'))`),
+
+  updatedAt: integer("updated_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(strftime('%s', 'now'))`)
+    .$onUpdateFn(() => sql`(strftime('%s', 'now'))`)
 };
 
 export { timestamps };
