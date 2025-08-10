30bebf26c42318ce4609110160c953072676a3c5
Ryan Luong <lumintriet@gmail.com>
2025-08-10 10:08:41 +0700

    Add email password sign in server function.

--- features/auth/server-functions/email-password.ts
+++ features/auth/server-functions/email-password.ts
@@ -4,7 +4,10 @@ import { redirect } from "next/navigation";
 import { APIError } from "better-auth/api";
 import * as z from "zod";
 import { auth } from "@/features/auth";
-import { emailPasswordSignUpSchema } from "@/features/auth/validators/email-password";
+import {
+  emailPasswordSignInSchema,
+  emailPasswordSignUpSchema
+} from "@/features/auth/validators/email-password";
 
 async function signUpEmailPassword(_initialState: unknown, formData: FormData) {
   const rawFields = Object.fromEntries(formData.entries());
@@ -33,4 +36,31 @@ async function signUpEmailPassword(_initialState: unknown, formData: FormData) {
   redirect("/admin");
 }
 
-export { signUpEmailPassword };
+async function signInEmailPassword(_initialState: unknown, formData: FormData) {
+  const rawFields = Object.fromEntries(formData.entries());
+  const result = emailPasswordSignInSchema.safeParse(rawFields);
+
+  if (!result.success) {
+    const errorTree = z.treeifyError(result.error);
+    return errorTree;
+  }
+
+  try {
+    await auth.api.signInEmail({
+      body: {
+        ...result.data
+      }
+    });
+  } catch (error) {
+    if (error instanceof APIError) {
+      return { errors: [error.message], properties: {} };
+    } else {
+      console.error("Unexpected error during sign in:", error);
+      return { errors: ["Something went wrong"], properties: {} };
+    }
+  }
+
+  redirect("/admin");
+}
+
+export { signUpEmailPassword, signInEmailPassword };
