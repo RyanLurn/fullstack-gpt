dfd35a7c92ae35e46cafe986496545961b1fe0c8
Ryan Luong <lumintriet@gmail.com>
2025-08-14 11:25:05 +0700

    Add typographical styles to markdown component.

--- features/chat/components/message/content/text-part/index.tsx
+++ features/chat/components/message/content/text-part/index.tsx
@@ -1,8 +1,53 @@
-import Markdown from "react-markdown";
+import Markdown, { type Options } from "react-markdown";
 import remarkGfm from "remark-gfm";
+import {
+  TypographyH1,
+  TypographyH2,
+  TypographyH3,
+  TypographyH4
+} from "@/components/typography/headings";
 import { cn } from "@/lib/utils";
 
+const markdownComponents: Options["components"] = {
+  strong: ({ children, className, ...props }) => (
+    <span className={cn("font-semibold", className)} {...props}>
+      {children}
+    </span>
+  ),
+  a: ({ children, className, ...props }) => (
+    <a
+      className={cn("font-medium text-primary underline", className)}
+      rel="noreferrer"
+      target="_blank"
+      {...props}
+    >
+      {children}
+    </a>
+  ),
+  h1: ({ children, className, ...props }) => (
+    <TypographyH1 className={cn("", className)} {...props}>
+      {children}
+    </TypographyH1>
+  ),
+  h2: ({ children, className, ...props }) => (
+    <TypographyH2 className={cn("", className)} {...props}>
+      {children}
+    </TypographyH2>
+  ),
+  h3: ({ children, className, ...props }) => (
+    <TypographyH3 className={cn("", className)} {...props}>
+      {children}
+    </TypographyH3>
+  ),
+  h4: ({ children, className, ...props }) => (
+    <TypographyH4 className={cn("", className)} {...props}>
+      {children}
+    </TypographyH4>
+  )
+};
+
 function TextPart({ className, text }: { className?: string; text: string }) {
+  console.log(text);
   return (
     <div
       className={cn(
@@ -10,7 +55,9 @@ function TextPart({ className, text }: { className?: string; text: string }) {
         className
       )}
     >
-      <Markdown remarkPlugins={[remarkGfm]}>{text}</Markdown>
+      <Markdown components={markdownComponents} remarkPlugins={[remarkGfm]}>
+        {text}
+      </Markdown>
     </div>
   );
 }
