3f8340bf8b1c0deafa25ff3048774ae03b048fdc
Ryan Luong <lumintriet@gmail.com>
2025-08-14 11:35:50 +0700

    Add list for typography.

---
+++ components/typography/list.tsx
@@ -0,0 +1,38 @@
+import { cn } from "@/lib/utils";
+
+function TypographyOrderedList({
+  className,
+  children
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <ol
+      className={cn(
+        "my-6 ml-6 list-inside list-decimal [&>li]:mt-2",
+        className
+      )}
+    >
+      {children}
+    </ol>
+  );
+}
+
+function TypographyUnorderedList({
+  className,
+  children
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <ul
+      className={cn("my-6 ml-6 list-inside list-disc [&>li]:mt-2", className)}
+    >
+      {children}
+    </ul>
+  );
+}
+
+export { TypographyOrderedList, TypographyUnorderedList };
