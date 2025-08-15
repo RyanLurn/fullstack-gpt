b18e24659b56a93cdbc0b5feb709a00836fc7e1a
Ryan Luong <lumintriet@gmail.com>
2025-08-15 08:14:21 +0700

    Move auth check to server functions.

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,6 +1,3 @@
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
-import { auth } from "@/features/auth";
 import { Chat } from "@/features/chat/components/chat";
 import { getChat } from "@/features/chat/db-operations/get-chat";
 
@@ -9,14 +6,7 @@ export default async function ChatPage({
 }: {
   params: Promise<{ id: string }>;
 }) {
-  const session = await auth.api.getSession({
-    headers: await headers()
-  });
-
-  if (!session?.user) {
-    redirect("/sign-in");
-  }
   const { id } = await params;
-  const chat = await getChat({ id, userId: session.user.id });
+  const chat = await getChat({ id });
   return <Chat id={chat.id} initialMessages={chat.messages} />;
 }
--- features/chat/db-operations/get-chat.ts
+++ features/chat/db-operations/get-chat.ts
@@ -1,19 +1,35 @@
-import { notFound } from "next/navigation";
+import { headers } from "next/headers";
+import { notFound, redirect } from "next/navigation";
 import { and, eq } from "drizzle-orm";
 import { db } from "@/database";
 import { chat } from "@/database/schemas/chat";
+import { auth } from "@/features/auth";
+import { appLogger } from "@/lib/logger";
 
-async function getChat({ id, userId }: { id: string; userId: string }) {
+async function getChat({ id }: { id: string }) {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+
+  if (!session?.user) {
+    appLogger.warn("Unauthorized attempt to get a chat via Server Action.");
+    redirect("/sign-in");
+  }
+
+  appLogger.info(
+    { userId: session.user.id },
+    "Getting chat via Server Action."
+  );
   const foundChat = await db.query.chat.findFirst({
-    where: and(eq(chat.id, id), eq(chat.userId, userId)),
+    where: and(eq(chat.id, id), eq(chat.userId, session.user.id)),
     with: {
       messages: true
     }
   });
 
   if (foundChat === undefined) {
-    console.error(
-      `Chat with id "${id}" not found or user "${userId}" does not have access.`
+    appLogger.error(
+      `Chat with id "${id}" not found or user "${session.user.id}" does not have access.`
     );
     notFound();
   }
